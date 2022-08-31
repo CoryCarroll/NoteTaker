753a29e2635aea0b19552a7d615b2377c455b053
CorysPC <corystory2@gmail.com>
2022-08-30 21:15:06 -0600

    adding middleware

--- Develop/server.js
+++ Develop/server.js
@@ -2,4 +2,31 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Assigning local port
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+app.get('/api/notes/:id', (req, res) => {
+    let dbNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(dbNotes[Number(req.params.id)]);
+});
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
