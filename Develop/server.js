db3ef6ed7165cb0d0a5357f619474a0b98273e65
CorysPC <corystory2@gmail.com>
2022-09-01 08:08:31 -0600

    server.js file complete

--- Develop/server.js
+++ Develop/server.js
@@ -30,3 +30,39 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.post('/api/notes', (req, res) => {
+  let dbNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let incomingNote = req.body;
+  let id = dbNotes.length.toString();
+  incomingNote.id = id;
+  dbNotes.push(incomingNote);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(dbNotes));
+  res.json(dbNotes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  let dbNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let incomingNoteId = req.params.id;
+  let id = 0;
+  dbNotes = dbNotes.filter((note) => {
+    return note.id != incomingNoteId;
+  });
+
+  for(note of dbNotes) {
+    note.id = id.toString();
+    id++;
+  }
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(dbNotes));
+  res.json(dbNotes);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
